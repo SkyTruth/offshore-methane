687d1e1245202a89f486ebfb5d656e45ce9ef699
jonaraphael <jona.raphael@gmail.com>
2025-06-10 09:34:55 -0400

    Refactor sentinel2 utils and expand tests

--- offshore_methane/utils.py
+++ offshore_methane/utils.py
@@ -1,68 +1,52 @@
 # Imports
+import datetime
 import ee
 import geemap
-import datetime
+
+
+def parse_sentinel2_id(id: str) -> tuple[str, datetime.datetime]:
+    """Return filter key and acquisition date for a Sentinel-2 identifier."""
+    if id.startswith(("S2A", "S2B")):
+        date_str = id.split("_")[2].split("T")[0]
+        key = "PRODUCT_ID"
+    elif id.startswith("L1C"):
+        date_str = id.split("_")[3].split("T")[0]
+        key = "GRANULE_ID"
+    else:
+        date_str = id.split("_")[0].split("T")[0]
+        key = "system:index"
+
+    date_obj = datetime.datetime(
+        int(date_str[0:4]), int(date_str[4:6]), int(date_str[6:])
+    )
+    return key, date_obj
+
+
+def fetch_sentinel2_image(key: str, id: str, date: datetime.datetime):
+    """Fetch an EE image collection filtered by identifier and date."""
+    orig_date = ee.Date(date)
+    return (
+        ee.ImageCollection("COPERNICUS/S2_HARMONIZED")
+        .filterDate(orig_date, orig_date.advance(1, "day"))
+        .filter(ee.Filter.eq(key, id))
+    )
+
+
+def create_sentinel2_map(collection, id: str):
+    """Return a ``geemap.Map`` for the provided image collection."""
+    m = geemap.Map()
+    m.addLayer(collection, {"bands": ["B4", "B3", "B2"], "min": 0, "max": 3000}, id)
+    m.centerObject(collection, 12)
+    return m
 
 # Pass in a Sentinel-2 L1C (TOA) EE Scene ID, Product ID, or Granule ID.
 # Returns the image on a geemap.
 def sentinel2_geemap(id):
-    
-    # Cases where the PRODUCT_ID is provided.
-    # Ex: S2B_MSIL1C_20240101T000749_N0510_R130_T50CND_20240101T004831 
-    if id.startswith(('S2A','S2B')):
+    """Return a ``geemap.Map`` showing the requested Sentinel-2 scene."""
+    key, date = parse_sentinel2_id(id)
+    collection = fetch_sentinel2_image(key, id, date)
 
-        imDate = id.split('_')[2].split('T')[0]
+    if collection.size().getInfo() == 0:
+        return "Image not found in Sentinel-2 TOA repo."
 
-        imDate_form = datetime.datetime(int(imDate[0:4]),int(imDate[4:6]),int(imDate[6:]))
-
-        orig_date = ee.Date(imDate_form)
-
-        s2Image = (ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
-                .filterDate(orig_date,orig_date.advance(1,'day'))
-                .filter(ee.Filter.eq('PRODUCT_ID',id)))
-
-        map = geemap.Map()
-        map.addLayer(s2Image,{'bands':['B4','B3','B2'],'min':0,'max':3000},id)
-        map.centerObject(s2Image,12)
-
-    # Cases where the GRANULE_ID is provided.
-    # Ex: L1C_T50CND_A035620_20240101T000751
-    elif id.startswith('L1C'):
-            
-        imDate = id.split('_')[3].split('T')[0]
-        
-        imDate_form = datetime.datetime(int(imDate[0:4]),int(imDate[4:6]),int(imDate[6:]))
-
-        orig_date = ee.Date(imDate_form)
-
-        s2Image = (ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
-                .filterDate(orig_date,orig_date.advance(1,'day'))
-                .filter(ee.Filter.eq('GRANULE_ID',id)))
-        
-        map = geemap.Map()
-        map.addLayer(s2Image,{'bands':['B4','B3','B2'],'min':0,'max':3000},id)
-        map.centerObject(s2Image,12)
-    
-    # This will capture the scene IDs, in theory.
-    # 20230611T162839_20230611T164034_T16RBT
-    else:
-        
-        imDate = id.split('_')[0].split('T')[0]
-        
-        imDate_form = datetime.datetime(int(imDate[0:4]),int(imDate[4:6]),int(imDate[6:]))
-
-        orig_date = ee.Date(imDate_form)
-
-        s2Image = (ee.ImageCollection('COPERNICUS/S2_HARMONIZED')
-                .filterDate(orig_date,orig_date.advance(1,'day'))
-                .filter(ee.Filter.eq('system:index',id)))
-    
-        map = geemap.Map()
-        map.addLayer(s2Image,{'bands':['B4','B3','B2'],'min':0,'max':3000},id)
-        map.centerObject(s2Image,12)
-        
-
-    if s2Image.size().getInfo() == 0:
-        return 'Image not found in Sentinel-2 TOA repo.'
-    else:
-        return map
+    return create_sentinel2_map(collection, id)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,63 @@
-from offshore_methane.utils import sentinel2_geemap
+import datetime
+from offshore_methane import utils
 
 
-def test_sentinel2_geemap_callable():
-    """Ensure the sentinel2_geemap function is defined and callable."""
-    assert callable(sentinel2_geemap)
+def test_parse_sentinel2_id_product():
+    key, date = utils.parse_sentinel2_id(
+        "S2A_MSIL1C_20240101T000749_N0510_R130_T50CND_20240101T004831"
+    )
+    assert key == "PRODUCT_ID"
+    assert date == datetime.datetime(2024, 1, 1)
+
+
+def test_parse_sentinel2_id_granule():
+    key, date = utils.parse_sentinel2_id("L1C_T50CND_A035620_20240101T000751")
+    assert key == "GRANULE_ID"
+    assert date == datetime.datetime(2024, 1, 1)
+
+
+def test_parse_sentinel2_id_scene():
+    key, date = utils.parse_sentinel2_id("20230611T162839_20230611T164034_T16RBT")
+    assert key == "system:index"
+    assert date == datetime.datetime(2023, 6, 11)
+
+
+def test_sentinel2_geemap_returns_map(monkeypatch):
+    class DummySize:
+        def getInfo(self):
+            return 1
+
+    class DummyCollection:
+        def size(self):
+            return DummySize()
+
+    def fake_fetch(key, id, date):
+        return DummyCollection()
+
+    def fake_create(collection, id):
+        return "dummy_map"
+
+    monkeypatch.setattr(utils, "fetch_sentinel2_image", fake_fetch)
+    monkeypatch.setattr(utils, "create_sentinel2_map", fake_create)
+
+    result = utils.sentinel2_geemap("20230611T162839_20230611T164034_T16RBT")
+    assert result == "dummy_map"
+
+
+def test_sentinel2_geemap_handles_missing(monkeypatch):
+    class DummySize:
+        def getInfo(self):
+            return 0
+
+    class DummyCollection:
+        def size(self):
+            return DummySize()
+
+    def fake_fetch(key, id, date):
+        return DummyCollection()
+
+    monkeypatch.setattr(utils, "fetch_sentinel2_image", fake_fetch)
+    monkeypatch.setattr(utils, "create_sentinel2_map", lambda *args, **kwargs: None)
+
+    result = utils.sentinel2_geemap("20230611T162839_20230611T164034_T16RBT")
+    assert result == "Image not found in Sentinel-2 TOA repo."
